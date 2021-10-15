8356b2868715d1d934d72c52569c29dc91a68c58
Tanya Zhuge <tangnazhuge@gmail.com>
2021-10-14 18:00:06 -0700

    Update and edit Game class

--- src/Game.js
+++ src/Game.js
@@ -1,13 +1,29 @@
 const data = require('./data');
 const prototypeQuestions = data.prototypeData;
 const util = require('./util');
+const Round = require('../src/Round.js');
+const Deck = require('../src/Deck.js');
+const Card = require('../src/Card.js');
 
 class Game {
-  constructor() {}
+  constructor() {
+    this.currentRound;
+  }
 
   printMessage(deck, round) {
       console.log(`Welcome to FlashCards! You are playing with ${deck.countCards()} cards.
 -----------------------------------------------------------------------`)
+  }
+
+  start(){
+    const dataCards = prototypeQuestions.map((element) => {
+      const card = new Card(element.id, element.question, element.answers, element.correctAnswer)
+      return card;
+    })
+    const deck = new Deck(dataCards);
+    this.currentRound = new Round(deck);
+    this.printMessage(deck, this.currentRound);
+    this.printQuestion(this.currentRound);
   }
 
   printQuestion(round) {
